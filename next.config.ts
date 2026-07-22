b4fc0d6db08c532ea5273ad468b0b5a7a8a43e81
mjbalcueva-prt <288203817+mjbalcueva-prt@users.noreply.github.com>
2026-07-22 15:44:37 +0800

    initial commit: base-next-template setup

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next"
+
+import { env } from "@/env"
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactCompiler: true,
+}
+
+export default nextConfig
